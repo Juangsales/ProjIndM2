34b33bb2c77b47eee7e37047ef17204e537dcfe5
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-10-09 13:19:02 -0300

    adicionado seleção

--- js/script.js
+++ js/script.js
@@ -1,66 +1,77 @@
-//Variaveis
 
-var base64 = document.getElementById('base')
-var cifracesar = document.getElementById('cesar')
+// Aparecer caixa de seleção para o metodo
+
+var msg = document.getElementById('msg')
+var metodo = document.getElementById('codigo')
+msg.addEventListener('click', function() {
+    metodo.style.display = 'block';
+})
+
+//Aparecer incremento para Cifra e os radios para ambos 
 var incremento = document.getElementById('number')
 var codiedecodi = document.getElementById('box0')
+var incrementar = addEventListener('click', function(){
+    var codigo = document.getElementById('codigo').value
+    if (codigo == "cifra") {
+    incremento.style.display  = 'block';
+    } else if (codigo == "base") { 
+    codiedecodi.style.display ='block';
+    
+    }
+})
+incremento.addEventListener('click', function(){
+    codiedecodi.style.display = 'block';
+})
+
+//Aparecer botão Decodificar ou Codificar
 var codificar = document.getElementById('codifi')
 var decodificar = document.getElementById('decodifi')
 var botaocod = document.getElementById('buttoncod')
 var botaodecod = document.getElementById('buttondecod')
-
-
-//Seelcionando Base64
-base64.addEventListener('click', function(b) {
-    codiedecodi.style.display = 'block'
-})
-
-//Selecionando Cifra de Cesar e colocar incremento
-cifracesar.addEventListener('click', function(cc) {
-    incremento.style.display = 'block'
-})
-incremento.addEventListener('click', function(i) {
-    codiedecodi.style.display = 'block'
-})
-
-//Aparecer botão de codificar e decodificar
-codificar.addEventListener('click', function(bc) {
+codificar.addEventListener('click', function(){
     botaocod.style.display = 'block'
+    botaodecod.style.display = 'none'
 })
-decodificar.addEventListener('click', function(bd) {
+decodificar.addEventListener('click', function() {
     botaodecod.style.display = 'block'
+    botaocod.style.display = 'none'
 })
 
 //Função Base64
-function codBase64() {
-  var mensagem = document.getElementById('msg')
-.value
-        var codificado = btoa(mensagem)
-        return codificado
+function codBase64 () {
+    var codigo = document.getElementById('msg').value
+    codi = atob(codigo)
+    return codi
 }
-    function decBase64(){
-      var mensagem = document.getElementById('msg')
-.value
-      var decodificado = atob(mensagem)
-        return decodificado
+function decBase64 () {
+    var codigo = document.getElementById('msg').value
+    decod = btoa(codigo)
+    return decod
 }
 
-//Trabalhar com a mensagem
-//No Base64
+//Aparecer mensagem codificada e decodificada
+//Codificada
 
-botaocod.addEventListener('click', function(c) {
-   c.preventDefault();
-    if (base64.checked && codificar.checked) {
-    resultado.innerHTML = codBase64();
-    resultado.style.display = 'block'
-  }
-})
+var resultado = document.getElementById('resultado')
+botaocod.addEventListener('click', function(e){
+    e.preventDefault();
+    if(codificar.checked && codigo.value === "base"){
+        resultado.innerText = codBase64();
+        resultado.style.display = 'block'
+    } else if(codificar.checked && codigo.value === "cesar"){
+        resultado.innerText = codCesar()
+        resultado.style.display = 'block'
+        }
+    })
+//Decodificada
 
-  botaodecod.addEventListener('click', function(dc) {
-    dc.preventDefault();
-    if (base64.checked && decodificar.checked) {
-    resultado.innerHTML = decBase64();
-      resultado.style.display = 'block'
-  } 
+botaodecod.addEventListener('click', function(f) {
+    f.preventDefault();
+    if(decodificar.checked && codigo.value === "cifra"){
+        resultado.innerText = decCesar()
+        resultado.style.display = 'block'
+    } else if(decodificar.checked && codigo.value === "base"){
+        resultado.innerText = decBase64();
+        resultado.style.display = 'block'
+    }
 })
-//no Cifra de Cesar
