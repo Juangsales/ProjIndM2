8e4fcf0c986f42a5d1cdf170c5e7b4cf84f191fd
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-10-14 20:40:11 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Aparecer caixa de seleção para o metodo
 
 var msg = document.getElementById('msg')
-var metodo = document.getElementById('codigo')
+var metodo = document.getElementById('box')
 msg.addEventListener('click', function() {
-    metodo.style.display = 'flex';
+    metodo.style.display = 'block';
 })
 
 //Aparecer incremento para Cifra e os radios para ambos 
